f950d8c0d04385ec5a8f328bd3720afbdc7baaed
anzhiyu <2268025923@qq.com>
2022-10-12 11:16:29 +0800

    Site updated: 2022-10-12 11:16:29

--- sw.js
+++ sw.js
@@ -20,7 +20,7 @@ workbox.core.clientsClaim();
 
 // 注册成功后要立即缓存的资源列表
 // 具体缓存列表在gulpfile.js中配置，见下文
-workbox.precaching.precacheAndRoute([{"revision":"5a3ec53dfbffd111451a0b97f0d8043f","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"74fde1c62e295d9bacf0967182715d23","url":"./css/index.css"}], {
+workbox.precaching.precacheAndRoute([{"revision":"5d9146f3279bb846b6ff2e2c49dac8b7","url":"./index.html"},{"revision":"73e1a92da3a39646678bffb9e7817148","url":"./js/main.js"},{"revision":"74fde1c62e295d9bacf0967182715d23","url":"./css/index.css"}], {
   directoryIndex: null,
 });
 
